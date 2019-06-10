2e036f2b9fdcf6637681eb439ba3d302266f134d
Annie Truex <artruex@gmail.com>
2019-06-10 19:45:50 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,8 +14,14 @@ def first_challenge
   }
 
   #your code here
-contacts["Freddie Mercury"][:favorite_icecream_flavors].delete_if"strawberry"
-puts contacts
+contacts.each do |person, data|
+  data.each do |key, value|
+    if key == :favorite_icecream_flavors
+      value.delete_if do |flavor|
+        flavor == "strawberry"
+          end
+      end
+  end
   #remember to return your newly altered contacts hash!
   contacts
 end
